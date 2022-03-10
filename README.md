6c8d36a9457f74b3e19a49fe769ccb1a5b89cadf
Ronny Weiss <30749177+RonnyWeiss@users.noreply.github.com>
2022-03-10 20:18:34 +0100

    change default allowed attributes for sanitizer and update sanitizer

--- README.md
+++ README.md
@@ -15,3 +15,5 @@ https://apex.oracle.com/pls/apex/f?p=103428
 If you like my stuff, would be nice if you donate me a coffee
 
 [![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.me/RonnyW1)
+
+**Important clarification: My work in the development team of Oracle APEX is in no way related to my open source projects or the plug-ins on apex.world! All plug-ins are built in my spare time and are not supported by Oracle!**
--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var notificationMenu = (function () {
     var util = {
         featureDetails: {
             name: "APEX Notification Menu",
-            scriptVersion: "1.6.4",
+            scriptVersion: "1.6.5",
             utilVersion: "1.4",
             url: "https://github.com/RonnyWeiss",
             license: "MIT"
@@ -134,7 +134,7 @@ var notificationMenu = (function () {
             /* this is the default json for purify js */
             var sanitizeConfigJSON;
             var stdSanatizerConfigJSON = {
-                "ALLOWED_ATTR": ["accesskey", "align", "alt", "always", "autocomplete", "autoplay", "border", "cellpadding", "cellspacing", "charset", "class", "dir", "height", "href", "id", "lang", "name", "rel", "required", "src", "style", "summary", "tabindex", "target", "title", "type", "value", "width"],
+                "ALLOWED_ATTR": ["accesskey", "align", "alt", "always", "autocomplete", "autoplay", "border", "cellpadding", "cellspacing", "charset", "class", "colspan", "dir", "height", "href", "id", "lang", "name", "rel", "required", "rowspan", "src", "style", "summary", "tabindex", "target", "title", "type", "value", "width"],
                 "ALLOWED_TAGS": ["a", "address", "b", "blockquote", "br", "caption", "code", "dd", "div", "dl", "dt", "em", "figcaption", "figure", "h1", "h2", "h3", "h4", "h5", "h6", "hr", "i", "img", "label", "li", "nl", "ol", "p", "pre", "s", "span", "strike", "strong", "sub", "sup", "table", "tbody", "td", "th", "thead", "tr", "u", "ul"]
             };
 
